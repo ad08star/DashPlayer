8f7e00bee09ea594bef6deee12dbf5c4e1d93903
Amandeep Singh <amandeep.singh40@ad.infosys.com>
2019-05-01 00:32:12 +0530

    Updates

---
+++ precache-manifest.7f1e6d8fb5bb5ee88ec50d842a6b6d53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DashPlayer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "530743e9d4734f9ef59b",
+    "url": "/DashPlayer/static/js/runtime~main.cda4c35c.js"
+  },
+  {
+    "revision": "22b24d65a207870de5d0",
+    "url": "/DashPlayer/static/js/main.d2ac993c.chunk.js"
+  },
+  {
+    "revision": "88b68ff732579c785efe",
+    "url": "/DashPlayer/static/js/2.a3c159f3.chunk.js"
+  },
+  {
+    "revision": "22b24d65a207870de5d0",
+    "url": "/DashPlayer/static/css/main.408a42b2.chunk.css"
+  },
+  {
+    "revision": "79b7d3fde2a8cb143e70a7bc55628b97",
+    "url": "/DashPlayer/index.html"
+  }
+];
